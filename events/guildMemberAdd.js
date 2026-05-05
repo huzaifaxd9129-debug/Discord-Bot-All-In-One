a0a25603843ae5b72a6612518d4d63d47a1ed4e2
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:28:37 +0500

    guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,17 @@
 module.exports = {
   name: "guildMemberAdd",
-  execute(client, member) {
-    client.systems.welcome.send(member);
+
+  async execute(client, member) {
+
+    // ================= INVITES TRACKING =================
+    if (client.systems?.invites) {
+      client.systems.invites.track(member);
+    }
+
+    // ================= WELCOME SYSTEM =================
+    if (client.systems?.welcome) {
+      client.systems.welcome.send(member);
+    }
+
   }
 };
