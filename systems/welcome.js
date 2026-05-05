388bc6938fe2a58ae3ac48f236445854ef0d889b
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:12:08 +0500

    welcome.js

--- systems/welcome.js
+++ systems/welcome.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 
+const configPath = path.join(__dirname, '../data/config.json');
+
+module.exports = {
+  async send(member) {
+    const data = JSON.parse(fs.readFileSync(configPath));
+
+    if (!data.welcomeChannel) return;
+
+    const channel = member.guild.channels.cache.get(data.welcomeChannel);
+    if (!channel) return;
+
+    channel.send(`Welcome ${member} to **${member.guild.name}** 🎉`);
+  }
+};
