446db7b837aa0444424ebf8344ebb37cb55bd096
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:16:13 +0500

    automod.js

--- systems/automod.js
+++ systems/automod.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  run(message) {
+    if (/(https?:\/\/)/gi.test(message.content)) {
+      message.delete().catch(() => {});
+      message.channel.send(`${message.author}, links not allowed.`);
+    }
+  }
+};
