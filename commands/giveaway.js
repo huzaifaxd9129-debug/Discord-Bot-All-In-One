b277897a376f6f5843d4c4d0ff14c68110e2e719
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:21:29 +0500

    giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,1 +1,10 @@
+module.exports = {
+  name: "giveaway",
+  async execute(interaction, client) {
+    const duration = 10000; // 10 sec test
+    const prize = "Free Nitro";
 
+    await interaction.reply("Giveaway started!");
+    client.systems.giveaway.start(interaction.channel, duration, prize);
+  }
+};
