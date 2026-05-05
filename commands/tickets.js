49e956f85e4c90f0f4691003d1252e4d0b33a1cd
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:22:56 +0500

    tickets.js

--- commands/tickets.js
+++ commands/tickets.js
@@ -1,1 +1,30 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
+module.exports = {
+  name: "tickets",
+
+  async execute(interaction) {
+
+    const embed = new EmbedBuilder()
+      .setTitle("🎫 Ticket System")
+      .setDescription("Click the button below to create a ticket")
+      .setColor("Blue");
+
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("create_ticket")
+        .setLabel("Create Ticket")
+        .setStyle(ButtonStyle.Primary),
+
+      new ButtonBuilder()
+        .setCustomId("close_ticket")
+        .setLabel("Close Ticket")
+        .setStyle(ButtonStyle.Danger)
+    );
+
+    await interaction.reply({
+      embeds: [embed],
+      components: [row]
+    });
+  }
+};
