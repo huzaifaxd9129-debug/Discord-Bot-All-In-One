ff72718c5c41b73f345246e6e58c265aff37b833
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:14:01 +0500

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,21 @@
+module.exports = {
+  name: "ready",
+  execute(client) {
+    console.log(`${client.user.tag} is online`);
 
+    const statuses = [
+      "👑 Made By Huztro",
+      "🔥 Use /help For More Info",
+      `${client.guilds.cache.size} servers`
+    ];
+
+    let i = 0;
+    setInterval(() => {
+      client.user.setPresence({
+        activities: [{ name: statuses[i % statuses.length] }],
+        status: "online"
+      });
+      i++;
+    }, 5000);
+  }
+};
