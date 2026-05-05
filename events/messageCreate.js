eae7a514fcdb849aaa67f41786639783a09ce42c
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:14:34 +0500

    messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,10 @@
+module.exports = {
+  name: "messageCreate",
+  async execute(client, message) {
+    if (message.author.bot) return;
 
+    client.systems.automod.run(message);
+    client.systems.autoresponder.run(message);
+    client.systems.autoreact.run(message);
+  }
+};
