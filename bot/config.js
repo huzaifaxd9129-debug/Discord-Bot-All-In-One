e320a0b5d0cb9d379fabdd1a7e21c8f8e657e15b
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:10:39 +0500

    config.js

--- bot/config.js
+++ bot/config.js
@@ -1,1 +1,13 @@
+module.exports = {
+  token: process.env.TOKEN,
 
+  clientId: process.env.CLIENT_ID,
+
+  prefix: "!",
+
+  colors: {
+    main: "#5865F2",
+    error: "#ED4245",
+    success: "#57F287"
+  }
+};
