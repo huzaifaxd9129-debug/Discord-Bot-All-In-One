922f10aa1c08478225223fed0e7f3fb3d939cfc4
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:23:34 +0500

    tickets.js

--- systems/tickets.js
+++ systems/tickets.js
@@ -1,6 +1,7 @@
 const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
+
   async create(interaction) {
     const channel = await interaction.guild.channels.create({
       name: `ticket-${interaction.user.username}`,
@@ -16,6 +17,18 @@ module.exports = {
       ]
     });
 
-    return channel;
+    await interaction.reply({ content: `Ticket created: ${channel}`, ephemeral: true });
+  },
+
+  async close(interaction) {
+    const channel = interaction.channel;
+
+    if (!channel.name.startsWith("ticket-")) {
+      return interaction.reply({ content: "This is not a ticket channel", ephemeral: true });
+    }
+
+    await interaction.reply("Closing ticket...");
+    setTimeout(() => channel.delete(), 3000);
   }
+
 };
