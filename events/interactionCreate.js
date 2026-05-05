ebb57808db243b1d6296252e7023377182e0652c
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:15:19 +0500

    interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,17 @@
+module.exports = {
+  name: "interactionCreate",
+  async execute(client, interaction) {
 
+    if (interaction.isChatInputCommand()) {
+      const cmd = client.commands.get(interaction.commandName);
+      if (cmd) cmd.execute(interaction, client);
+    }
+
+    if (interaction.isButton()) {
+      if (interaction.customId === "create_ticket") {
+        const channel = await client.systems.tickets.create(interaction);
+        interaction.reply({ content: `Created: ${channel}`, ephemeral: true });
+      }
+    }
+  }
+};
