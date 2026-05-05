6ba6c73871c8e7448135af218fbdba26180b8238
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-05 23:11:38 +0500

    tickets.js

--- systems/tickets.js
+++ systems/tickets.js
@@ -1,1 +1,21 @@
+const { PermissionsBitField } = require('discord.js');
 
+module.exports = {
+  async create(interaction) {
+    const channel = await interaction.guild.channels.create({
+      name: `ticket-${interaction.user.username}`,
+      permissionOverwrites: [
+        {
+          id: interaction.guild.id,
+          deny: [PermissionsBitField.Flags.ViewChannel]
+        },
+        {
+          id: interaction.user.id,
+          allow: [PermissionsBitField.Flags.ViewChannel]
+        }
+      ]
+    });
+
+    return channel;
+  }
+};
